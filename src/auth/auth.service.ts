7bad815cf0439d013bc0e90c88257a7df2fc25a3
chaeyn <chaulfe@gmail.com>
2025-09-10 23:17:08 +0900

    refactor: 비밀번호 암호화, 로그인 로직 개선

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import { SignInUserDto } from './dto/signin-user.dto';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable() // 의존성 주입 가능한 서비스
 export class AuthService {
@@ -12,14 +18,18 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async createUser(data: CreateUserDto): Promise<User> {
+  async createUser(data: CreateUserDto) {
     // Promise<User>는 비동기적으로 User 객체를 반환함을 의미
     try {
-      const user = await this.prisma.user.create({
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+      await this.prisma.user.create({
         // create는 nest가 제공하는 메서드
-        data,
+        data: {
+          ...data,
+          password: hashedPassword,
+        },
       });
-      return user;
+      return;
     } catch (error) {
       throw new HttpException(
         `회원가입 실패, ${error.message}`,
@@ -31,20 +41,22 @@ export class AuthService {
   async signin(data: SignInUserDto) {
     const user = await this.prisma.user.findUnique({
       // findUnique는 고유한 값을 찾을 때 사용
-      where: { phone: data.phone, password: data.password },
+      where: { phone: data.phone },
     });
-    if (user) {
-      const payload = { phone: user.phone, name: user.name }; // JWT에 담길 정보
-      const accessToken = this.jwtService.sign(payload); // JWT 토큰 생성
-      return accessToken;
-    } else {
-      throw new HttpException(
-        {
-          success: false,
-          message: '로그인 실패, 아이디 또는 비밀번호를 확인하세요.',
-        },
-        HttpStatus.UNAUTHORIZED,
-      );
+
+    if (!user) {
+      throw new UnauthorizedException('아이디 또는 비밀번호를 확인하세요.'); // Unauthorized -> 401, 인증 실패
     }
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+    // bcrypt.compare는 해시된 비밀번호와 원래 비밀번호를 비교
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('아이디 또는 비밀번호를 확인하세요.');
+    }
+
+    const payload = { sub: user.id, phone: user.phone, name: user.name }; // JWT에 담길 정보, sub는 subject의 약자, 토큰의 주체를 나타낸다
+    const accessToken = this.jwtService.sign(payload); // JWT 토큰 생성
+    return accessToken;
   }
 }
