b407006e1177784f155730617ebc497a18a31e1c
chaeyn <chaulfe@gmail.com>
2025-09-13 23:26:03 +0900

    fix: ConfigModule undefined 에러

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
+    ConfigModule,
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
