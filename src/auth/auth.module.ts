5df8915d8744d23f5f9ac08077e43c2f2cb952c3
chaeyn <chaulfe@gmail.com>
2025-09-13 23:22:18 +0900

    fix: ConfigModule 중복 import 문제 해결

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, ConfigModule],
+  imports: [PrismaModule],
   controllers: [ReportController],
   providers: [ReportService],
 })
